daa9fa854421d54f318e720712750fe31f901da0
JJnight-wg <80240450+JJnight-wg@users.noreply.github.com>
2025-12-06 17:52:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,23 @@
 window.onload = () => {
   document.getElementById("button").onclick = () => {
-    window.YaAuthSuggest.init(
-      {
-        client_id: "y0__xCHy8KUBhjEpjUgq8u7xhUw38yBuQgBARdyXUzQzK6Jb3sCRTN24kwWHw",
-        response_type: "token",
-        redirect_uri: "https://oauth-master-class-cofk.vercel.app/token.html",
-      },
-      "https://oauth-master-class-cofk.vercel.app",
-      {
-        view: "button",
-        parentId: "buttonContainer",
-        buttonSize: "m",
-        buttonView: "main",
-        buttonTheme: "light",
-        buttonBorderRadius: "0",
-        buttonIcon: "ya",
-      }
-    )
-       .then(({ handler }) => handler())
+   window.onload = () => {
+    document.getElementById("suggest").onclick = () => {
+        YaAuthSuggest.init(
+            {
+                client_id: "y0__xCHy8KUBhjEpjUgq8u7xhUw38yBuQgBARdyXUzQzK6Jb3sCRTN24kwWHw",
+                response_type: "token",
+                redirect_uri: "https://oauth-master-class-cofk.vercel.app//token.html",
+            },
+            "https://oauth-master-class-cofk.vercel.app/"
+        )
+        .then(({ handler }) => handler())
         .then(async (data) => {
-        const result = await fetchYandexData(data.access_token);
+            const result = await fetchYandexData(data.access_token);
 
-        authorize(result);
+            authorize(result);
 
-        console.log(result, data);
-      })
-      .catch((error) => console.log("Что-то пошло не так: ", error));
+            console.log(result, data);
+        })
+        .catch((error) => console.log("Что-то пошло не так: ", error));
+    };
+};
