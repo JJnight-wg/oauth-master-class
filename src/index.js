535e301bf6e0cb9dae85b938f553e0cb5609fd8e
JJnight-wg <80240450+JJnight-wg@users.noreply.github.com>
2025-12-06 18:25:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,61 +1,59 @@
 const authorize = ({
-    default_avatar_id: defaultAvatarId,
-    display_name: displayName,
+  default_avatar_id: defaultAvatarId,
+  display_name: displayName,
 }) => {
-    const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
-    const nameHtml = `<div class="name">${displayName}</div>`;
-
-    document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
+  const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
+  const nameHtml = `<div class="name">${displayName}</div>`;
+  document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
 };
 
 const fetchYandexData = (token) =>
-    fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`)
-        .then((res) => res.json());
+  fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`)
+    .then((res) => res.json());
 
 window.onload = () => {
+  document.getElementById("suggest").onclick = () => {
+    YaAuthSuggest.init(
+      {
+        client_id: "930b6d7f5f79450faf49d6a221c15bf2",
+        response_type: "token",
+        redirect_uri: "https://oauth-master-class-cofk.vercel.app/token.html",
+      },
+      "https://oauth-master-class-cofk.vercel.app"
+    )
+      .then(({ handler }) => handler())
+      .then(async (data) => {
+        const result = await fetchYandexData(data.access_token);
+        authorize(result);
+        console.log(result, data);
+      })
+      .catch((error) => console.log("Что-то пошло не так: ", error));
+  };
 
-    document.getElementById("suggest").onclick = () => {
-        YaAuthSuggest.init(
-            {
-                client_id: "930b6d7f5f79450faf49d6a221c15bf2",
-                response_type: "token",
-                redirect_uri: "https://oauth-master-class-cofk.vercel.app/token.html",
-            },
-            "https://oauth-master-class-cofk.vercel.app"
-        )
-        .then(({ handler }) => handler())
-        .then(async (data) => {
-            const result = await fetchYandexData(data.access_token);
-            authorize(result);
-            console.log(result, data);
-        })
-        .catch((error) => console.log("Что-то пошло не так: ", error));
-    };
-
-    document.getElementById("button").onclick = () => {
-        window.YaAuthSuggest.init(
-            {
-                client_id: "f8c0fb137ddc4bd2acea1a1489b5ae1b",
-                response_type: "token",
-                redirect_uri: "https://oauth-master-class.vercel.app/token2.html",
-            },
-            "https://oauth-master-class.vercel.app",
-            {
-                parentId: "buttonContainer",
-                view: "button",
-                buttonTheme: "light",
-                buttonSize: "xs",
-                buttonBorderRadius: 20,
-            }
-        )
-        .then(({ handler }) => handler())
-        .then(async (data) => {
-            console.log("Сообщение с токеном(от кнопки): ", data);
-            const result = await fetchYandexData(data.access_token);
-            console.log("Сообщение с ответом Яндекса(от кнопки): ", result);
-            authorize(result);
-        })
-        .catch((error) => console.log("Что-то пошло не так: ", error));
-    };
+  document.getElementById("button").onclick = () => {
+    window.YaAuthSuggest.init(
+      {
+        client_id: "f8c0fb137ddc4bd2acea1a1489b5ae1b",
+        response_type: "token",
+        redirect_uri: "https://oauth-master-class.vercel.app/token2.html",
+      },
+      "https://oauth-master-class.vercel.app",
+      {
+        parentId: "buttonContainer",
+        view: "button",
+        buttonTheme: "light",
+        buttonSize: "xs",
+        buttonBorderRadius: 20,
+      }
+    )
+      .then(({ handler }) => handler())
+      .then(async (data) => {
+        console.log("Сообщение с токеном(от кнопки): ", data);
+        const result = await fetchYandexData(data.access_token);
+        console.log("Сообщение с ответом Яндекса(от кнопки): ", result);
+        authorize(result);
+      })
+      .catch((error) => console.log("Что-то пошло не так: ", error));
+  };
 
 };
