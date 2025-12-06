b8709f657d5d6b5bb6b73e28374389d491403ef7
JJnight-wg <80240450+JJnight-wg@users.noreply.github.com>
2025-12-06 18:13:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,13 +22,13 @@ window.onload = () => {
             },
             "https://oauth-master-class-cofk.vercel.app"
         )
-        .then(({ handler }) => handler())
-        .then(async (data) => {
+            .then(({ handler }) => handler())
+            .then(async (data) => {
             const result = await fetchYandexData(data.access_token);
 
             authorize(result);
 
             console.log(result, data);
-        })
-        .catch((error) => console.log("Что-то пошло не так: ", error));
+                })
+            .catch((error) => console.log("Что-то пошло не так: ", error));
     };
